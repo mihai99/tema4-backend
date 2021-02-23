12ace4ed04e3cb5dbd94f8e1de5f56826e7f7642
Gitea <gitea@fake.local>
2021-02-23 19:05:43 +0200

    last edits

--- hw1/script/script.py
+++ hw1/script/script.py
@@ -2,20 +2,27 @@ from threading import Thread
 import sys
 from queue import Queue
 import requests
+import time
 
 url = 'http://localhost:3000/request'
 myobj = {'title': 'cat'}
 
 
-concurrent = 50
-total = 200
-
+concurrent = 5
+total = 100
+concurentArray = []
+meanTime = 0
 def concurentRequests():
+    global meanTime
     while True:
-        url = q.get()
+        nr = q.get()
+        start = time.time()
         res = getResponse(url)
+        end = time.time()
+        meanTime += start - end
         outputResult(res)
         q.task_done()
+    print("mean time:", meanTime/total)
 
 def getResponse(ourl):
     try:
@@ -25,7 +32,11 @@ def getResponse(ourl):
         return "error"
 
 def outputResult(res):
-    print(res)
+    global concurentArray
+    concurentArray.append(res)
+    if (len(concurentArray) == concurrent):
+        print(concurentArray)
+        concurentArray = []
 
 q = Queue(concurrent)
 
@@ -33,9 +44,10 @@ for i in range(concurrent):
     t = Thread(target=concurentRequests)
     t.daemon = True
     t.start()
+
 try:
     for i in range(total):
-        q.put(url)
+        q.put(i)
     q.join()
 except KeyboardInterrupt:
     sys.exit(1)
