81fcc0927660633b4f4a90f5cea5923deba99539
Gitea <gitea@fake.local>
2021-03-29 20:20:34 +0300

    +front

--- hw3/my-app/src/App.js
+++ hw3/my-app/src/App.js
@@ -1,12 +1,33 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+// import Login from "./components/Login";
+import useToken from "./utils/useToken";
+import UniqueBoard from "./components/UniqueBoard";
 
 function App() {
+  const { token, setToken } = useToken();
+
+  // if (!token) {
+  //   return <Login setToken={setToken} />;
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Go fuck yourself</p>
-      </header>
+    <div className="wrapper" style={{ height: "100%" }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/board/:id">
+            <UniqueBoard />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+          {/* <Route path="/preferences">
+            <Preferences />
+          </Route> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
